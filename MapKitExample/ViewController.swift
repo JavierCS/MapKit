05a2d2b882d873ce56ddd2ab1fefb4ade87b881f
Javier Cruz Santiago <javier.cruz@icomsys.com.mx>
2019-02-06 10:16:41 -0600

    Initial Commit

---
+++ MapKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitExample
+//
+//  Created by Javier Cruz Santiago on 2/6/19.
+//  Copyright © 2019 Javier Cruz Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
